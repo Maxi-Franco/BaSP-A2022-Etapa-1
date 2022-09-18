1c714e087fd1707f0575bc4a805adb847439e50c
Maxi Franco <mfrancos810@gmail.com>
2022-09-18 00:57:32 -0300

    BaSP-W04: ex04-if else

--- Semana-04/exercises/04_if_else.js
+++ Semana-04/exercises/04_if_else.js
@@ -1,1 +1,54 @@
-console.log('Console log desde 04 js');
+console.log('--EXERCISE 4: IF ELSE');
+console.log('----------------');
+
+/* a.
+  Crear un número aleatorio entre 0 y 1 utilizando la función Math.random(), 
+  si el valor es mayor o igual que 0,5 mostrar una alerta con el mensaje 
+  “Greater than 0,5” y sino un alerta con el mensaje “Lower than 0,5”.
+*/
+console.log('-Exercise 4.a:');
+
+var random4a = Math.random();
+
+if (random4a >= 0.5){
+  alert('Greater than 0,5');
+} else {
+  alert('Lower than 0,5');
+}
+
+console.log('Número random: ' + random4a);
+console.log('----------------');
+
+/* b.
+  Crear una variable “Age” que contenga un número entero entre 0 y 100 y 
+  muestre los siguientes mensajes de alerta:
+    1.  “Bebe” si la edad es menor a 2 años;
+    2.  “Niño” si la edad es entre 2 y 12 años;
+    3.  “Adolescente” entre 13 y 19 años;
+    4.  “Joven” entre 20 y 30 años;
+    5.  “Adulto” entre 31 y 60 años;
+    6.  “Adulto mayor” entre 61 y 75 años;
+    7.  “Anciano” si es mayor a 75 años.
+*/
+console.log('-Exercise 4.b:');
+
+var age = 75;
+
+if (age < 2){
+  alert('Bebe');
+} else if (age < 13){
+  alert('Niño');
+} else if (age < 20){
+  alert('Adolescente');
+} else if (age < 31){
+  alert('Joven');
+} else if (age < 61){
+  alert('Adulto');
+} else if (age <= 75){
+  alert('Adulto mayor');
+} else if (age > 75){
+  alert('Anciano');
+}
+
+console.log(age);
+console.log('----------------');
